cfdae93d98a7e98c4d97624662fafb821e66e6fe
mchaillou <mchaillou@carnot.planet>
2015-11-16 14:47:52 +0100

    Initial Commit

---
+++ MCSlidingTabs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCSlidingTabs
+//
+//  Created by mchaillou on 16/11/2015.
+//  Copyright © 2015 Maxime Chaillou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCSlidingTabs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCSlidingTabs
+//
+//  Created by mchaillou on 16/11/2015.
+//  Copyright © 2015 Maxime Chaillou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
